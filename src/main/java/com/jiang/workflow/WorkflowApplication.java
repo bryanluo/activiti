9495c9780a8b952457107666c87f5927cee58788
shijiang.luo <shijiang.luo@gziiim.com>
2020-12-24 17:30:03 +0800

    集成流程设计器和swagger

--- src/main/java/com/jiang/workflow/WorkflowApplication.java
+++ src/main/java/com/jiang/workflow/WorkflowApplication.java
@@ -1,20 +1,25 @@
 package com.jiang.workflow;
 
-import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author shijiang
- *
  */
+@EnableSwagger2
+
+
 @MapperScan("com.jiang.workflow.domain.repository")
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.activiti.spring.boot.SecurityAutoConfiguration.class
+})
 public class WorkflowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WorkflowApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowApplication.class, args);
+    }
 
 }

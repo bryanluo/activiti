7db118dc272808d46a31f1c9238415b39d48543b
shijiang.luo <shijiang.luo@gziiim.com>
2020-12-25 14:01:22 +0800

    解决设计模型不能提交问题

--- src/main/java/com/jiang/workflow/editor/model/ModelSaveRestResource.java
+++ src/main/java/com/jiang/workflow/editor/model/ModelSaveRestResource.java
@@ -14,6 +14,7 @@ package com.jiang.workflow.editor.model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.RequiredArgsConstructor;
 import org.activiti.editor.constants.ModelDataJsonConstants;
 import org.activiti.engine.ActivitiException;
 import org.activiti.engine.RepositoryService;
@@ -23,9 +24,7 @@ import org.apache.batik.transcoder.TranscoderOutput;
 import org.apache.batik.transcoder.image.PNGTranscoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.ByteArrayInputStream;
@@ -41,35 +40,32 @@ import java.io.InputStream;
  */
 @RestController
 @RequestMapping(value = "/service")
+@RequiredArgsConstructor
 public class ModelSaveRestResource implements ModelDataJsonConstants {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(ModelSaveRestResource.class);
-
-    @Autowired
-    private RepositoryService repositoryService;
-
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final RepositoryService repositoryService;
+    private final ObjectMapper objectMapper;
 
     @RequestMapping(value = "/model/{modelId}/save", method = RequestMethod.PUT)
     @ResponseStatus(value = HttpStatus.OK)
-    public void saveModel(@PathVariable String modelId, @RequestBody MultiValueMap<String, String> values) {
+    public void saveModel(@PathVariable String modelId, String name, String description, String json_xml, String svg_xml) {
         try {
 
             Model model = repositoryService.getModel(modelId);
 
             ObjectNode modelJson = (ObjectNode) objectMapper.readTree(model.getMetaInfo());
 
-            modelJson.put(MODEL_NAME, values.getFirst("name"));
-            modelJson.put(MODEL_DESCRIPTION, values.getFirst("description"));
+            modelJson.put(MODEL_NAME, name);
+            modelJson.put(MODEL_DESCRIPTION, description);
             model.setMetaInfo(modelJson.toString());
-            model.setName(values.getFirst("name"));
+            model.setName(name);
 
             repositoryService.saveModel(model);
 
-            repositoryService.addModelEditorSource(model.getId(), values.getFirst("json_xml").getBytes("utf-8"));
+            repositoryService.addModelEditorSource(model.getId(), json_xml.getBytes("utf-8"));
 
-            InputStream svgStream = new ByteArrayInputStream(values.getFirst("svg_xml").getBytes("utf-8"));
+            InputStream svgStream = new ByteArrayInputStream(svg_xml.getBytes("utf-8"));
             TranscoderInput input = new TranscoderInput(svgStream);
 
             PNGTranscoder transcoder = new PNGTranscoder();
